b5d25c636c427f40df72f700a4d8369c48e06b56
cuongdev <cuongdev@nexondv.com>
2023-04-05 15:35:48 +0700

    fix socket io

--- src/configs/environments..ts
+++ src/configs/environments..ts
@@ -3,10 +3,10 @@ require("dotenv").config();
 export const env = {
   MONGODB_URI: process.env.MONGODB_URI,
   HOST: process.env.HOST,
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 8080,
   TOKEN_URL: process.env.TOKEN_URL || "",
   CLIENT_ID: process.env.CLIENT_ID || "",
   CLIENT_SECRET: process.env.CLIENT_SECRET || "",
   GRANT_TYPE: process.env.GRANT_TYPE || "",
-  PLAYLIST_URL: process.env.PLAYLIST_URL || ""
+  PLAYLIST_URL: process.env.PLAYLIST_URL || "",
 };
